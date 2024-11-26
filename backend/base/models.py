ad59a966117c637cbd41f597d6abdd558761f72a
Maggie Kairu <susangideon@Maggies-Pro.local>
2024-11-26 10:29:48 +0300

    this is the base of the notes app that has djangorestframework and jwt authentication features

---
+++ backend/base/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# class Note(models.Model):
+#     description = models.TextField(max_length=200)
+#     owner = models.ForeignKey
+
